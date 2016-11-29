34629d8e62b44bff251286905cc19908ff8058d6
gavbn <wjw69763@xzsok.com>
2016-11-29 14:24:03 +0530

    Update server_search.go

--- server/server_search.go
+++ server/server_search.go
@@ -54,6 +54,19 @@ func (s *Server) fetchSearchConfig() error {
 //see github.com/jpillora/scraper for config specification
 //cloud-torrent uses "<id>-item" handlers
 var defaultSearchConfig = []byte(`{
+	"tpb": {
+		"name": "The Pirate Bay",
+		"url": "https://thepiratebay.org/search/{{query}}/{{page:0}}/7//",
+		"list": "#searchResult > tbody > tr",
+		"result": {
+			"name":"a.detLink",
+			"path":["a.detLink","@href"],
+			"magnet": ["a[title=Download\\ this\\ torrent\\ using\\ magnet]","@href"],
+			"size": "/Size (\\d+(\\.\\d+).[KMG]iB)/",
+			"seeds": "td:nth-child(3)",
+			"peers": "td:nth-child(4)"
+		}
+	},
 	"et": {
 		"name": "ExtraTorrent",
 		"url": "https://extratorrent.cc/search/?search={{query}}&s_cat=&pp=&srt=seeds&order=desc&page={{page:1}}",
@@ -61,7 +74,7 @@ var defaultSearchConfig = []byte(`{
 		"result": {
 			"name":["td.tli > a"],
 			"torrent": ["td:nth-child(1) a","@href","s/torrent_download/download/"],
-			"url": ["td.tli > a","@href"],
+			"path": ["td.tli > a","@href"],
 			"size": "td:nth-child(5)",
 			"seeds": "td.sy",
 			"peers": "td.ly"
@@ -94,7 +107,7 @@ var defaultSearchConfig = []byte(`{
 	"eztv": {
 		"name": "EZTV",
 		"url": "https://eztv.ag/search/{{query}}",
-		"list": "table tr.forum_header_border",
+		"list": "#header_holder > table:nth-child(13) tr.forum_header_border",
 		"result": {
 			"name": "td:nth-child(2) a",
 			"url": ["td:nth-child(2) a", "@href"],
@@ -106,20 +119,20 @@ var defaultSearchConfig = []byte(`{
 	"1337x": {
 		"name": "1337X",
 		"url": "http://1337x.to/sort-search/{{query}}/seeders/desc/{{page:1}}/",
-		"list": ".box-info-detail table.table tr",
+		"list": ".search-result ul.clearfix > li",
 		"result": {
-			"name":[".coll-1 a:nth-child(2)"],
-			"url":[".coll-1 a:nth-child(2)", "@href"],
+			"name":[".coll-1 strong a"],
+			"item":[".coll-1 strong a", "@href"],
 			"seeds": ".coll-2",
 			"peers": ".coll-3",
-			"size": [".coll-4", "/([\\d\\.]+ [KMGT]?B)/"]
+			"size": ".coll-4"
 		}
 	},
 	"1337x/item": {
 		"name": "1337X (Item)",
 		"url": "http://1337x.to{{item}}",
 		"result": {
-			"magnet": ["a.btn-magnet","@href"]
+			"magnet": ["#magnetdl","@href"]
 		}
 	},
 	"abb": {
@@ -128,7 +141,7 @@ var defaultSearchConfig = []byte(`{
 		"list": "#content > div",
 		"result": {
 			"name":["div.postTitle > h2 > a","@title"],
-			"url":["div.postTitle > h2 > a","@href"],
+			"item":["div.postTitle > h2 > a","@href"],
 			"seeds": "div.postContent > p:nth-child(3) > span:nth-child(1)",
 			"peers": "div.postContent > p:nth-child(3) > span:nth-child(3)"
 		}
